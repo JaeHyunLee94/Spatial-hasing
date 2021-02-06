64d614558a4552c7c965137d2beb70d47b106861
LEE-JAE-HYUN179 <leejaehyun1223@gmail.com>
2021-02-06 16:33:29 +0900

    add test case 2

--- CollisionDetector/main.cpp
+++ CollisionDetector/main.cpp
@@ -8,28 +8,25 @@ int main(int argc, char** argv) {
 	CollisionDetector detector;
 
 	MockingMesh mesh1;
-	mesh1.vertices.push_back(glm::vec3(0, 0, 0));
-	mesh1.vertices.push_back(glm::vec3(1.2, 0, 0));
-	mesh1.vertices.push_back(glm::vec3(0, 1.2, 0));
-	mesh1.vertices.push_back(glm::vec3(0, 0, 1.2));
-	mesh1.vertices.push_back(glm::vec3(1.2, 1.2, 1.2));
+	mesh1.vertices.push_back(glm::vec3(1.1, 1.1, 1.1));
+	mesh1.vertices.push_back(glm::vec3(3.2, 5.4, 5.4));
+	mesh1.vertices.push_back(glm::vec3(5.4, 3.2, 5.4));
+	mesh1.vertices.push_back(glm::vec3(5.4, 5.4, 3.2));
+	
 
 	mesh1.tet.push_back(MockingTet{
 			0,1,2,3
 		}
 	);
-	mesh1.tet.push_back(MockingTet{
-			1,2,3,4
-		}
-	);
+	
 
 
 	MockingMesh mesh2;
 	
-	mesh2.vertices.push_back(glm::vec3(-0.4, 0, 0));
-	mesh2.vertices.push_back(glm::vec3(0, -0.4, 0));
-	mesh2.vertices.push_back(glm::vec3(0, 0, -0.4));
-	mesh2.vertices.push_back(glm::vec3(0.1, 0.1, 0.1));
+	mesh2.vertices.push_back(glm::vec3(2.1, 2.1, 2.1));
+	mesh2.vertices.push_back(glm::vec3(2.5, 2.9, 2.9));
+	mesh2.vertices.push_back(glm::vec3(2.9, 2.5, 2.9));
+	mesh2.vertices.push_back(glm::vec3(2.9, 2.9, 2.5));
 
 	mesh2.tet.push_back(MockingTet{
 			0,1,2,3
@@ -48,18 +45,24 @@ int main(int argc, char** argv) {
 	glm::vec3 t4(0,0,0);
 
 
+	std::cout << "output collision vector size : " << outPut.size() << "\n";
+	for (int i = 0; i < outPut.size(); i++) {
+
+		std::cout << "penetrated mesh: " << outPut[i].penetratedMesh << '\n';
+		std::cout << "penetrating mesh: " << outPut[i].penetratingMesh << '\n';
+		std::cout << "vertex list: \n";
+		for (int j = 0; j < outPut[i].verticeList.size(); j++) {
+			std::cout << outPut[i].verticeList[j] << " ";
+
+		}
+		std::cout << "\n";
+
+
+
+
+	}
+
 	
-	std::cout << outPut.size() << "\n";
-	std::cout << outPut[0].penetratedMesh << "\n";
-	std::cout << outPut[0].penetratingMesh << "\n";
-	std::cout << outPut[0].verticeList.size() << "\n\n";
-
-	for (int i = 0; i < outPut[0].verticeList.size(); i++)std::cout << outPut[0].verticeList[i] << "\n\n";
-
-	std::cout << outPut[1].penetratedMesh << "\n";
-	std::cout << outPut[1].penetratingMesh << "\n";
-	std::cout << outPut[1].verticeList.size() << "\n\n";
-	for(int i=0;i<outPut[1].verticeList.size();i++)std::cout << outPut[1].verticeList[i] << "\n";
 
 	
 
